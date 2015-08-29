e3ecbaa39a756a6c7f5433bbd984245c2fc466bf
PedroLDN <pedrofrankstein13@hotmail.com>
2015-08-29 11:44:25 -0300

    Site de amostra para o currúculo concluído - BootstrapZika.com

---
+++ js/jscript.js
@@ -0,0 +1,40 @@
+﻿(function($){
+
+	$(document).ready(function(){
+		
+		function changePositionProp(){
+			
+			var offsetTop = $("body").scrollTop();
+			
+			if(offsetTop > 0){
+				$("#navbar").css("position", "fixed");
+			}
+			else{
+				$("#navbar").css("position", "static");
+			}
+			
+			time = null;
+			
+		}
+		
+		var time = null;
+		
+		$(this).scroll(function(event){
+					
+			time = null;
+			
+			time = setTimeout(changePositionProp, 40);
+			
+		});
+		
+		$("a[href^='#']").click(function(e){
+			
+			e.preventDefault();
+			$($(this).attr("href"))[0].scrollIntoView();
+			scrollBy(0, -150);
+		
+		})
+		
+	})
+
+}(jQuery));
